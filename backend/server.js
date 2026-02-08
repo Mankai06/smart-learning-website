fa141225d9ac9699823652522e743d6beba2f111
Mankai06 <shreedevamankai06@gmail.com>
2026-02-08 13:25:39 +0530

    fixed booking request waiting

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,12 @@ app.use(cors({
 
 /* -------------------- MongoDB -------------------- */
 /* PASTE YOUR OWN CONNECTION STRING HERE */
-mongoose.connect("mongoose.connect(process.env.MONGO_URI);")
-.then(() => console.log("MongoDB Atlas Connected (ONLINE DATABASE)"))
-.catch(err => console.log("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log("MongoDB Error:", err));
 
 /* -------------------- Booking Model -------------------- */
 const Booking = require("./models/Booking");
